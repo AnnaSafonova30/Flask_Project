d8cad0c8ab2b07fc5af1e6cf6d8181e28f4291cb
SviatMartynenko <s8888788@gmail.com>
2024-09-27 20:10:32 +0100

    add user.app

--- home/app.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-home_app = flask.Blueprint(
-    name = "home_app",
-    import_name = "home_app",
-    static_folder = "station",
-    static_url_path = "/station/",
-    template_folder = "templates"
-)
--- home/views.py
+++
@@ -1,4 +0,0 @@
-import flask
-
-def show_home_list():
-    return flask.render_template(template_name_or_list = "home.html")
